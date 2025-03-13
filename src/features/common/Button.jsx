804be5f6a659b424b0a3e74084493ca275e77c0b
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2025-03-14 01:20:20 +0200

    Update Button component

--- src/features/common/Button.jsx
+++ src/features/common/Button.jsx
@@ -4,10 +4,10 @@ import clsx from 'clsx';
 const Button = ({ className, text, onClick = null, span = '' }) => {
   return (
     <button className={clsx('btn', className)} onClick={onClick}>
-      <>
+      <span className="btn__content">
         {text}
         <span className={`${className}__text`}>{span}</span>
-      </>
+      </span>
     </button>
   );
 };
