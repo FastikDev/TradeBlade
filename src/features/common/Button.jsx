6622be545b9f09dd10445be643c47198f0b4b74c
FastikDev <sir.fastik18@gmail.com>
2025-03-14 02:57:48 +0200

    fix deploy

--- src/features/common/Button.jsx
+++ src/features/common/Button.jsx
@@ -4,10 +4,10 @@ import clsx from 'clsx';
 const Button = ({ className, text, onClick = null, span = '' }) => {
   return (
     <button className={clsx('btn', className)} onClick={onClick}>
-      <>
+      <div className="btn__content">
         {text}
         <span className={`${className}__text`}>{span}</span>
-      </>
+      </div>
     </button>
   );
 };
